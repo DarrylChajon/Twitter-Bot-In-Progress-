91ede945bf770487c433ecba326e72cb015cb6c3
DarrylChajon <126701455+DarrylChajon@users.noreply.github.com>
2025-02-22 02:02:28 -0500

    simple tweet

---
+++ simpletweetX.py
@@ -0,0 +1,15 @@
+import tweepy
+
+consumer_key = 'xxx'
+consumer_secret = 'xxx'
+access_token = 'xxx'
+access_secret = 'xxx'
+
+client = tweepy.Client(
+    consumer_key = consumer_key, consumer_secret = consumer_secret,
+    access_token=access_token, access_token_secret=access_secret)
+
+response = client.create_tweet(
+    text='Check check python post!')
+
+print("Successful post!")
